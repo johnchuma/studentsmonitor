5233af3f9347493f3a491d8a3bf54ae856968f89
johnchuma <johnvchuma@gmail.com>
2023-04-22 22:47:40 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/studentsmonitor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studentsmonitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/tracking_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TrackingPage extends StatefulWidget {
+  const TrackingPage({super.key});
+
+  @override
+  State<TrackingPage> createState() => _TrackingPageState();
+}
+
+class _TrackingPageState extends State<TrackingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
